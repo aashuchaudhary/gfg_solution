b0a5a06a60a5b563619a5b2c527cfa74d6629f77
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-09-16 00:27:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Tower Of Hanoi/tower-of-hanoi.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    void tohcal(int N, int sour, int help, int dest) {
+        if (N == 1) {
+            cout << "move disk " << N << " from rod " << sour << " to rod " << dest << endl;
+            return;
+        }
+        
+        tohcal(N-1, sour, dest, help);
+        cout << "move disk " << N << " from rod " << sour << " to rod " << dest << endl;
+        tohcal(N-1, help, sour, dest);
+    }
+
+    long long toh(int N, int from, int to, int aux) {
+        tohcal(N, from, aux, to);
+        return pow(2, N) - 1;
+    }
+
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int T;
+    cin >> T; // testcases
+    while (T--) {
+
+        int N;
+        cin >> N; // taking input N
+
+        // calling toh() function
+        Solution ob;
+
+        cout << ob.toh(N, 1, 3, 2) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
