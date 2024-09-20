e57012400dc9c05f1e6f3954390fa6d20f951b89
Ashutosh Chaudhary <123310232+aashuchaudhary@users.noreply.github.com>
2024-09-20 11:55:59 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Binary Search/binary-search.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int binarysearch(vector<int> &arr, int k) {
+        int start=0,end=arr.size()-1,mid,ans=-1;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            if(arr[mid]==k){
+                ans =mid;
+               break;
+            }
+            else if(arr[mid]<k) 
+                start=mid+1;
+            else
+             end=mid-1;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int k;
+        cin >> k;
+        vector<int> arr;
+        string input;
+        cin.ignore();
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int res = ob.binarysearch(arr, k);
+        cout << res << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
